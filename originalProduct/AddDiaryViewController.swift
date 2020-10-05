966e950175705af2b8624d510d730b48641c612f
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2020-10-05 12:00:17 +0900

    コア機能正直まだまだです

---
+++ originalProduct/AddDiaryViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AddDiaryViewController.swift
+//  originalProduct
+//
+//  Created by 伊藤明孝 on 2020/10/05.
+//
+
+import UIKit
+
+class AddDiaryViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+
+   @IBOutlet weak var titleTexitField: UITextField!
+   @IBOutlet weak var detailTextView: UITextView!
+   
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       
+       titleTexitField.delegate = self
+       detailTextView.delegate = self
+
+   }
+  
+   
+   // MARK: - TextField Delegate
+   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       textField.resignFirstResponder()
+       return true
+   }
+   
+   // MARK: - TextView Delegate
+   func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+       textView.resignFirstResponder()
+       return true
+   }
+   
+   @IBAction func save() {
+       if titleTexitField.text != "" {
+           if detailTextView.text != "" {
+               let newDiary = Diary.create()
+               newDiary.title = titleTexitField.text!
+               newDiary.note = detailTextView.text
+               let today = Diary.changeDateType(date: Date())
+               newDiary.date = today
+               newDiary.save()
+               self.navigationController?.popViewController(animated: true)
+           } else {
+               SimpleAlert.showAlert(viewController: self, title: "日記なし", message: "内容を描いてください", buttonTitle: "OK")
+           }
+       } else {
+           SimpleAlert.showAlert(viewController: self, title: "タイトルなし", message: "タイトルを書いてください", buttonTitle: "OK")
+       }
+       
+   }
+   
+}
---
+++ originalProduct/DetailDairyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailDairyViewController.swift
+//  originalProduct
+//
+//  Created by 伊藤明孝 on 2020/10/05.
+//
+
+import UIKit
+
+class DetailDairyViewController: UIViewController {
+   
+   var selectedDiary = Diary()
+   
+   @IBOutlet weak var detailTextView: UITextView!
+
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       
+       self.navigationController?.title = "\(selectedDiary.title)"
+       detailTextView.text = selectedDiary.note
+   }
+   
+
+}
